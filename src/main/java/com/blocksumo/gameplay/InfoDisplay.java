23695f85d7276d763d3055d99372ced6c64d84ea
24Kings <24kingsunite@gmail.com>
2023-08-29 12:04:27 -0700

    Concept for info display

---
+++ src/main/java/com/blocksumo/gameplay/InfoDisplay.java
@@ -0,0 +1,15 @@
+package com.blocksumo.gameplay;
+
+/** Scoreboard setup
+ * BLOCK SUMO
+ * ---------
+ * Kills: <Number of Kills>
+ *
+ * Sumo Lives: (updates from most -> least)
+ * 24Kings: <Number of Lives>
+ * Etc.
+ *
+ */
+
+public class InfoDisplay {
+}
